834e84caa3ed22576a265705a967d6789c8be9b5
Nolan Wispmother <nwispmot@mi-l5.21-school.ru>
2019-06-10 20:57:22 +0300

    aaaa

--- push_swap.c
+++ push_swap.c
@@ -31,26 +31,26 @@ void	ft_initialization(t_pu *pu)
 
 void	printstack(t_pu *pu)
 {
-	int i;
-	int t = pu->size_a > pu->size_b ? pu->size_a : pu->size_b;
-
-	i = -1;
-	ft_printf("----------------------------\n");
-	ft_printf("|%-11c ||" "%11c |\n", 'a', 'b');
-	ft_printf("----------------------------");
-	ft_printf("\n");
-	while (++i < t)
-	{
-		if (i < pu->size_a)
-			ft_printf("|%11ld |", pu->stack_a[i]);
-		else
-			ft_printf("|%11s |", " ");
-		if (i < pu->size_b)
-			ft_printf("|%11ld |\n", pu->stack_b[i]);
-		else
-			ft_printf("|%11s |\n", " ");
-	}
-	ft_printf("----------------------------\n\n");
+//	int i;
+//	int t = pu->size_a > pu->size_b ? pu->size_a : pu->size_b;
+//
+//	i = -1;
+//	ft_printf("----------------------------\n");
+//	ft_printf("|%-11c ||" "%11c |\n", 'a', 'b');
+//	ft_printf("----------------------------");
+//	ft_printf("\n");
+//	while (++i < t)
+//	{
+//		if (i < pu->size_a)
+//			ft_printf("|%11ld |", pu->stack_a[i]);
+//		else
+//			ft_printf("|%11s |", " ");
+//		if (i < pu->size_b)
+//			ft_printf("|%11ld |\n", pu->stack_b[i]);
+//		else
+//			ft_printf("|%11s |\n", " ");
+//	}
+//	ft_printf("----------------------------\n\n");
 	pu->operations++;
 }
 
@@ -127,81 +127,121 @@ void	do_operations(t_pu	*pu, t_swap	*swap)
 	while(swap->ra-- != 0)
 	{
 		rotate_operations(pu, 'a');
-		//swap->ra--;
 		ft_printf("ra\n");
 	}
 	while(swap->rb-- != 0)
 	{
 		rotate_operations(pu, 'b');
-		//swap->rb--;
 		ft_printf("rb\n");
 	}
 	while(swap->rr-- != 0)
 	{
 		rotate_operations(pu, 'r');
-		//swap->rr--;
 		ft_printf("rr\n");
 	}
 	while(swap->rra-- != 0)
 	{
 		reverse_rotate_operations(pu, 'a');
-		//swap->rra--;
 		ft_printf("rra\n");
 	}
 	while(swap->rrb-- != 0)
 	{
 		reverse_rotate_operations(pu, 'b');
-		//swap->rrb--;
 		ft_printf("rra\n");
 	}
 	while(swap->rrr-- != 0)
 	{
 		reverse_rotate_operations(pu, 'r');
-		//swap->rrr--;
 		ft_printf("rrr\n");
 	}
 }
 
+int sorted(t_pu	*pu)
+{
+	int i;
+
+	i = 0;
+	if ((pu->stack_a[0] == pu->min && pu->stack_a[pu->size_a - 1] == pu->max && pu->stack_a[1] > pu->stack_a[0]) ||
+	(pu->stack_a[0] == pu->max && pu->stack_a[pu->size_a - 1] < pu->stack_a[0] && pu->stack_a[1] == pu->min)
+	|| (pu->stack_a[1] >  pu->stack_a[0] && pu->stack_a[pu->size_a - 1] <  pu->stack_a[0]))
+		i++;
+	else
+		return (0);
+	while(i < pu->size_a - 1)
+	{
+		if (pu->stack_a[i] == pu->min && pu->stack_a[i - 1] == pu->max && pu->stack_a[i + 1]  > pu->stack_a[i])
+			i++;
+		if (pu->stack_a[i] == pu->max && pu->stack_a[i + 1] == pu->min && pu->stack_a[i - 1]  < pu->stack_a[i])
+			i++;
+		else if (pu->stack_a[i - 1] < pu->stack_a[i] && pu->stack_a[i + 1] > pu->stack_a[i])
+			i++;
+		else
+			return (0);
+	}
+	if ((pu->stack_a[pu->size_a - 1] == pu->min && pu->stack_a[pu->size_a - 2] == pu->max && pu->stack_a[pu->size_a - 1] < pu->stack_a[0]) ||
+		(pu->stack_a[pu->size_a - 1] == pu->max && pu->stack_a[pu->size_a - 2] < pu->stack_a[pu->size_a - 1] && pu->stack_a[0] == pu->min)
+		|| (pu->stack_a[pu->size_a - 1] > pu->stack_a[pu->size_a - 2] && pu->stack_a[pu->size_a - 1] < pu->stack_a[0]))
+		i = 0;
+	else
+		return (0);
+	while (pu->stack_a[i] != pu->min)
+		i++;
+	if	(i <= pu->size_a / 2)
+	{
+		while (pu->stack_a[0] != pu->min)
+		{
+			rotate_operations(pu, 'a');
+			ft_printf("ra\n");
+		}
+	}
+	else
+		while (pu->stack_a[pu->size_a - 1] != pu->max)
+		{
+			reverse_rotate_operations(pu, 'a');
+			ft_printf("rra\n");
+		}
+	exit (0);
+}
+
 void ft_push(t_pu	*pu)
 {
-	int begin;
-	int i = 0;
-	long pre;
-
-	begin = pu->stack_a[i];
-	pre = pu->stack_a[i];
-	rotate_operations(pu, 'a');
-	ft_printf("ra\n");
-	while (pu->stack_a[0] != pu->max)
-	{
-		if (pu->stack_a[0] > pre)
+		while(pu->size_a != 3)
 		{
-			pre = pu->stack_a[0];
-			rotate_operations(pu, 'a');
-			ft_printf("ra\n");
+			if ((pu->stack_a[0] == pu->max || pu->stack_a[0] == pu->min) && (pu->stack_b[0] < pu->stack_b[1]))
+			{
+				rotate_operations(pu, 'r');
+				ft_printf("rr\n");
+			} else if (pu->stack_a[0] == pu->max || pu->stack_a[0] == pu->min)
+			{
+				rotate_operations(pu, 'a');
+				ft_printf("ra\n");
+			}
+			else
+				{
+				push_operations(pu, 'b');
+				ft_printf("pb\n");
+				if (pu->stack_b[0] < pu->stack_b[1] && pu->stack_b[0] > pu->stack_b[2])
+				{
+					swap_operations(pu, 'b');
+					ft_printf("sb\n");
+				}
+			}
 		}
-		else {
-			push_operations(pu, 'b');
-			ft_printf("pb\n");
-		}
-
-	}
-	rotate_operations(pu, 'a');
-	ft_printf("ra\n");
-	pre = -2147483649;
-	while (pu->stack_a[0] != begin)
-	{
-		if (pu->stack_a[0] < begin && pu->stack_a[0] > pre)
+		if (pu->stack_a[1] < pu->stack_a[0] && pu->stack_a[1] > pu->stack_a[2])
 		{
-			pre = pu->stack_a[0];
-			rotate_operations(pu, 'a');
-			ft_printf("ra\n");
+			swap_operations(pu, 'a');
+			ft_printf("sa\n");
 		}
-		else {
-			push_operations(pu, 'b');
-			ft_printf("pb\n");
+		else if (pu->stack_a[1] < pu->stack_a[0] && pu->stack_a[1] < pu->stack_a[2])
+		{
+			swap_operations(pu, 'a');
+			ft_printf("pa\n");
 		}
-	}
+		else if (pu->stack_a[1] > pu->stack_a[0] && pu->stack_a[1] > pu->stack_a[2] && pu->stack_a[0] < pu->stack_a[2])
+		{
+			swap_operations(pu, 'a');
+			ft_printf("sa\n");
+		}
 }
 
 void ft_sort(t_pu	*pu)
@@ -213,25 +253,6 @@ void ft_sort(t_pu	*pu)
 
 	swap = (t_swap*)malloc(sizeof(t_swap));
 	i = 0;
-//	while(pu->size_a != 3)
-//	{
-//		if ((pu->stack_a[0] == pu->max || pu->stack_a[0] == pu->min) && (pu->stack_b[0] < pu->stack_b[1]))
-//			rotate_operations(pu, 'r');
-//		else if (pu->stack_a[0] == pu->max || pu->stack_a[0] == pu->min)
-//			rotate_operations(pu, 'a');
-//		else
-//		{
-//			push_operations(pu, 'b');
-//			if (pu->stack_b[0] < pu->stack_b[1] && pu->stack_b[0] > pu->stack_b[2])
-//				swap_operations(pu, 'b');
-//		}
-//	}
-//	if (pu->stack_a[1] < pu->stack_a[0] && pu->stack_a[1] > pu->stack_a[2])
-//		swap_operations(pu, 'a');
-//	else if (pu->stack_a[1] < pu->stack_a[0] && pu->stack_a[1] < pu->stack_a[2])
-//		swap_operations(pu, 'a');
-//	else if (pu->stack_a[1] > pu->stack_a[0] && pu->stack_a[1] > pu->stack_a[2] && pu->stack_a[0] < pu->stack_a[2])
-//		swap_operations(pu, 'a');
 	ft_push(pu);
 	while (pu->size_b > 0)
 	{
@@ -273,22 +294,13 @@ void ft_sort(t_pu	*pu)
 		ft_printf("pa\n");
 		i = 0;
 	}
-	while (pu->stack_a[i] != pu->min)
-		i++;
-	if	(i <= pu->size_a / 2)
+	if (sorted(pu) == 0)
 	{
-		while (pu->stack_a[0] != pu->min) {
-			rotate_operations(pu, 'a');
-			ft_printf("ra\n");
-		}
+		ft_printf("Error\n");
+		exit(0);
 	}
-	else
-		while (pu->stack_a[pu->size_a - 1] != pu->max)
-		{
-			reverse_rotate_operations(pu, 'a');
-			ft_printf("rra\n");
-		}
 }
+
 
 int		main(int ac, char **av)
 {
@@ -307,6 +319,8 @@ int		main(int ac, char **av)
 		ft_record(pu, av[count]);
 	ft_check_repeat(pu);
 	printstack(pu);
+	if (sorted(pu) != 0)
+		exit(0);
 	ft_sort(pu);
 	ft_printf("operations === %d\n", pu->operations);
 	exit (0);
